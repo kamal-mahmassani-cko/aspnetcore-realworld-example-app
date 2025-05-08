b203b68d66df68f8445d5b11dda84def9d7efb42
Kamal Mahmassani <66256462+kamal-mahmassani-cko@users.noreply.github.com>
2025-05-08 11:21:37 +0100

    Update src/Conduit/Features/Users/User.cs

--- src/Conduit/Features/Users/User.cs
+++ src/Conduit/Features/Users/User.cs
@@ -1,16 +1,7 @@
-namespace Conduit.Features.Users;
-
-public class User
-{
-    public string? Username { get; init; }
-
-    public string? Email { get; init; }
-
-    public string? Bio { get; init; }
 
     public string? Image { get; init; }
 
+    public string? Address { get; init; }
+
     public string? Token { get; set; }
 }
-
-public record UserEnvelope(User User);
