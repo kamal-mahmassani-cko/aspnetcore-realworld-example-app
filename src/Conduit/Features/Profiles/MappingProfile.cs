d9ddc3007ef473d380ce0d38a57a8bf0beb7dd87
Kamal Mahmassani <66256462+kamal-mahmassani-cko@users.noreply.github.com>
2025-05-08 11:21:35 +0100

    Update src/Conduit/Features/Profiles/MappingProfile.cs

--- src/Conduit/Features/Profiles/MappingProfile.cs
+++ src/Conduit/Features/Profiles/MappingProfile.cs
@@ -1,7 +1,3 @@
-using AutoMapper;
-
-namespace Conduit.Features.Profiles;
-
 public class MappingProfile : AutoMapper.Profile
 {
     public MappingProfile() => CreateMap<Domain.Person, Profile>(MemberList.None);
