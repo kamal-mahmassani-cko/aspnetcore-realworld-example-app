f6f21a9e67070c120436ca194b93f0769f75efed
Kamal Mahmassani <66256462+kamal-mahmassani-cko@users.noreply.github.com>
2025-05-08 11:21:34 +0100

    Update src/Conduit/Domain/Person.cs

--- src/Conduit/Domain/Person.cs
+++ src/Conduit/Domain/Person.cs
@@ -1,33 +1,7 @@
-using System.Collections.Generic;
-using System.Text.Json.Serialization;
-
-namespace Conduit.Domain;
-
-public class Person
-{
-    [JsonIgnore]
-    public int PersonId { get; init; }
-
-    public string? Username { get; set; }
-
-    public string? Email { get; set; }
-
-    public string? Bio { get; set; }
 
     public string? Image { get; set; }
 
+    public string? Address { get; set; }
+
     [JsonIgnore]
     public List<ArticleFavorite> ArticleFavorites { get; init; } = new();
-
-    [JsonIgnore]
-    public List<FollowedPeople> Following { get; init; } = new();
-
-    [JsonIgnore]
-    public List<FollowedPeople> Followers { get; init; } = new();
-
-    [JsonIgnore]
-    public byte[] Hash { get; set; } = [];
-
-    [JsonIgnore]
-    public byte[] Salt { get; set; } = [];
-}
