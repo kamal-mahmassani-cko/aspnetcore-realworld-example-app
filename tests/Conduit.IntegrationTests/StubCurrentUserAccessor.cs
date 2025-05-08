d767c3cadb607994ffcb8af848ecf053d06a2a97
Kamal Mahmassani <66256462+kamal-mahmassani-cko@users.noreply.github.com>
2025-05-08 11:21:41 +0100

    Update tests/Conduit.IntegrationTests/StubCurrentUserAccessor.cs

--- tests/Conduit.IntegrationTests/StubCurrentUserAccessor.cs
+++ tests/Conduit.IntegrationTests/StubCurrentUserAccessor.cs
@@ -1,7 +1,3 @@
-using Conduit.Infrastructure;
-
-namespace Conduit.IntegrationTests;
-
 public class StubCurrentUserAccessor(string userName) : ICurrentUserAccessor
 {
     public string GetCurrentUsername() => userName;
